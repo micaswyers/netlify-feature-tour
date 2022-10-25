0e7be7d423adfadca19c8b8d530cc87232baac81
Mica Swyers <mswyers@twilio.com>
2022-10-25 14:59:08 +0100

    Add API call to hello-world function

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,13 +1,13 @@
+const axios = require('axios');
+
 exports.handler = async () => {
   // Test making a request to an external API and displaying the response
-  const Http = new XMLHttpRequest();
-  const mySecret = process.env.MY_SECRET;
-  const url = "https://the-one-api.dev/v2/book";
-  Http.open("GET", url);
-  Http.send();
+  const Url = "https://the-one-api.dev/v2/book";
+  const response = await axios.get(Url);
+  const { docs } = response.data;
+
   return {
     statusCode: 200,
-    body: `hello world I have a ${mySecret}`,
-    book: Http.responseText,
+   body: `The first LOTR book is ${docs[0].name}`,
   };
 };
